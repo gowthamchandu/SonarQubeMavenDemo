ea0220c529653c960ae854ceb5665685e4092afe
gowthamchandu <77114064+gowthamchandu@users.noreply.github.com>
2021-02-18 19:51:07 +0530

    Add files via upload

---
+++ src/main/java/com/example/SonarQubeMavenDemo/SonarQubeMavenDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SonarQubeMavenDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarQubeMavenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarQubeMavenDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SonarQubeMavenDemo/SonarQubeMavenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SonarQubeMavenDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarQubeMavenDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
